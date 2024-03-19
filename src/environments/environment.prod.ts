32f77a55b76f255117fede237d830dda40734837
Johannes Zeller <jo.zeller@protonmail.com>
2024-03-19 19:55:03 +0100

    provide prod environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'TBD',
+  apiUrl: 'https://tough-vault-backend.jzel.online',
   siteTitle: 'TouGH Vault',
-  authUrl: 'TBD',
-  homeUrl: 'TBD',
-  installationUrl: 'TBD'
+  authUrl: 'https://tough-vault-keycloak.jzel.online/realms/TouGH-Vault',
+  homeUrl: 'https://tough-vault.jzel.online',
+  installationUrl: 'https://github.com/apps/tough-vault/installations/new'
 };
