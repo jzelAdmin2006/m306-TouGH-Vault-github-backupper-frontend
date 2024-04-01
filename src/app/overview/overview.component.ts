a5b613cfb6c333d70e133176c6c4316185faca1c
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-04-01 18:40:15 +0200

    automatically fetch repos instead of user having to refresh

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,34 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { RepoService } from '../model/repo.service';
 import { Repo } from '../model/repo';
-import { catchError, firstValueFrom, tap } from 'rxjs';
+import { catchError, switchMap, startWith, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'tghv-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, OnDestroy {
   repos: Repo[] = [];
   private readonly repoService: RepoService = inject(RepoService);
+  private subscription: Subscription = new Subscription();
 
-  async ngOnInit(): Promise<void> {
-    await firstValueFrom(
-      this.repoService.getAll().pipe( // TODO get repos every few seconds, update the isProtecting values only if the fetch time has been changed
+  ngOnInit(): void {
+    this.subscription.add(
+      interval(1000).pipe(
+        startWith(0),
+        switchMap(() => this.repoService.getAll()), // TODO update the isProtecting values only if the fetch time has been changed
         catchError((err) => {
           if (err.status === 404) {
             window.location.href = environment.authInitUrl;
             return [];
           } else {
-            console.log(err);
+            console.error(err);
             throw err;
           }
         }),
         tap((repos) => {
           this.repos = repos;
         })
-      )
+      ).subscribe()
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
