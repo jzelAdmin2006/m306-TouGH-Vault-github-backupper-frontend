5549f25e607a3f94a60a899fd05198018b30120b
Johannes Zeller <jo.zeller@protonmail.com>
2024-04-04 20:44:58 +0200

    prevent settings from being loaded from backend multiple times

--- src/app/model/settings.service.ts
+++ src/app/model/settings.service.ts
@@ -1,18 +1,36 @@
 import { inject, Injectable } from '@angular/core';
 import { SettingsRestService } from '../adapter/settings-rest.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, first, tap } from 'rxjs/operators';
+import { Settings } from './settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
+  private readonly settingsRestService = inject(SettingsRestService);
+  private settingsSubject = new BehaviorSubject<Settings | undefined>(undefined);
+  private loading = false;
 
-  private readonly settingsRestService: SettingsRestService = inject(SettingsRestService);
-
-  getSettings() {
-    return this.settingsRestService.getSettings();
+  getSettings(): Observable<Settings> {
+    if (!this.loading && !this.settingsSubject.getValue()) {
+      this.loading = true;
+      this.settingsRestService.getSettings().pipe(
+        tap(settings => {
+          this.settingsSubject.next(settings);
+          this.loading = false;
+        })
+      ).subscribe();
+    }
+    return this.settingsSubject.asObservable().pipe(
+      filter((settings): settings is Settings => settings !== undefined),
+      first()
+    );
   }
 
-  toggleSetting(settingEndpointSuffix: string) {
-    return this.settingsRestService.toggleSetting(settingEndpointSuffix);
+  toggleSetting(settingEndpointSuffix: string): Observable<Settings> {
+    return this.settingsRestService.toggleSetting(settingEndpointSuffix).pipe(
+      tap(settings => this.settingsSubject.next(settings))
+    );
   }
 }
