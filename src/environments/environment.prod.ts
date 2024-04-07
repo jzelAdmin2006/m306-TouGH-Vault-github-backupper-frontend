cd83d89e78873ee18a4e9ba5a55518033aba7cb4
Johannes Zeller <jo.zeller@protonmail.com>
2024-04-07 22:13:48 +0200

    add productive config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   siteTitle: 'TouGH Vault',
   authUrl: 'https://tough-vault-keycloak.jzel.online/realms/TouGH-Vault',
   homeUrl: 'https://tough-vault.jzel.online',
-  authInitUrl: 'TBD', // TODO
-  installationUrl: 'https://github.com/apps/tough-vault/installations/new',
+  authInitUrl: 'https://github.com/login/oauth/authorize?scope=user%3Aemail&state=TkK9HIGTgHcwRlOI9kWMBwGpwagvR1caRXI1V4fBGss.k6pteYObHpU.W11JAgrxRx-Ef_7Za8kWwQ&response_type=code&client_id=Iv1.1c1e277c2642363e&redirect_uri=https%3A%2F%2Ftough-vault.jzel.online%2Fcallback',
+  installationUrl: 'https://github.com/apps/tough-vault-demo-deployment/installations/new',
 };
