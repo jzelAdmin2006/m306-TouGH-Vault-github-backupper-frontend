981d41c0d02f142160a9d5d6e0e5e7415ae18a50
Mateusz Wnuk <101288018+Grandinho@users.noreply.github.com>
2024-05-16 16:44:00 +0200

    reverted port change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'http://localhost:8080',
   siteTitle: 'TouGH Vault DEV',
   authUrl: 'http://localhost:18080/realms/TouGH-Vault',
   homeUrl: 'http://localhost:4200',
